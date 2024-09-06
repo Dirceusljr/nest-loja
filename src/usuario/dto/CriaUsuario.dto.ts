8762f2850352d46db3590ebedab839f75b4973c2
Dirceusljr <dirceusljr@gmail.com>
2024-09-06 15:02:38 -0300

    feat: :sparkles: feat: Decorator EmailEhUnico

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,10 +1,12 @@
 import { IsEmail, IsNotEmpty, Min, MinLength } from "class-validator";
+import { EmailEhUnico } from "../validator/email-eh-unico.validator";
 
 export class CriaUsuarioDTO {
     @IsNotEmpty({ message: 'O nome não pode ser vazio.' })
     nome: string;
 
     @IsEmail(undefined, { message: 'E-mail inválido.' })
+    @EmailEhUnico({ message: 'Já existe um usuário com este e-mail' })
     email: string;
 
     @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
